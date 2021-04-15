b5d151dc422a4b27dac96cdef05500ed45e29945
Stefan Denchev <zirael@abv.bg>
2021-04-16 00:26:56 +0300

    Layout and Views Work

--- Web/Journey.Web/wwwroot/js/site.js
+++ Web/Journey.Web/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+    if (!$(this).next().hasClass('show')) {
+        $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+    }
+    var $subMenu = $(this).next('.dropdown-menu');
+    $subMenu.toggleClass('show');
+
+
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+        $('.dropdown-submenu .show').removeClass('show');
+    });
+
+
+    return false;
+});
