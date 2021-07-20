1e4b8d6439045a807cd52707bd9fb073de2322b4
Stefan Denchev <65227182+stefandenchev@users.noreply.github.com>
2021-07-20 20:17:04 +0300

    Combined the Votes for Game and Forum into one Controller

--- Web/Journey.Web/Controllers/VotesController.cs
+++ Web/Journey.Web/Controllers/VotesController.cs
@@ -4,6 +4,7 @@
     using System.Threading.Tasks;
 
     using Journey.Services.Data.Interfaces;
+    using Journey.Web.ViewModels.Forum.Votes;
     using Journey.Web.ViewModels.Votes;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
@@ -13,21 +14,37 @@
     public class VotesController : BaseController
     {
         private readonly IVotesService votesService;
+        private readonly IForumVotesService forumVotesService;
 
-        public VotesController(IVotesService votesService)
+        public VotesController(IVotesService votesService, IForumVotesService forumVotesService)
         {
             this.votesService = votesService;
+            this.forumVotesService = forumVotesService;
         }
 
         [Authorize]
         [HttpPost]
-        public async Task<ActionResult<GameVoteResponseModel>> Post(GameVoteInputModel input)
+        [Route("game")]
+
+        public async Task<ActionResult<GameVoteResponseModel>> GamePost(GameVoteInputModel input)
         {
             var userId = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
             await this.votesService.SetVoteAsync(input.GameId, userId, input.Value);
             var averageVotes = this.votesService.GetAverageVotes(input.GameId);
 
             return new GameVoteResponseModel { AverageVote = averageVotes };
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("forum")]
+
+        public async Task<ActionResult<ForumVoteResponseModel>> ForumPost(ForumVoteInputModel input)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            await this.forumVotesService.VoteAsync(input.PostId, userId, input.IsUpVote);
+            var votes = this.forumVotesService.GetVotes(input.PostId);
+            return new ForumVoteResponseModel { VotesCount = votes };
         }
     }
 }
